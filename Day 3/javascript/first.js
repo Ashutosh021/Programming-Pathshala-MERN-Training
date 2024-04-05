fb1f7bba78d8ecfdbf3a311f863c443a33ed728d
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2024-04-05 09:52:04 +0530

    Day - 3

---
+++ Day 3/javascript/first.js
@@ -0,0 +1,82 @@
+// console.log("Hii");
+
+// var msg = "hiii";
+// let pri = 5;
+// console.log(msg);
+
+// function env(){
+//     for(var i=0;i<5;i++){
+//         console.log(i);
+//     }
+//     console.log(" -> "+i);
+// }
+
+// env();
+
+// var msg = "byeee..."
+// const str = `Hii msg is : ${msg}`;
+
+// console.log(str);
+
+// function print(x){
+//     console.log(x);
+// }
+
+// var a=x=>{
+//     console.log(x);
+// }
+
+// var b=(c,d)=>{
+//     console.log(c+d);
+// }
+
+// var x = 20;
+// print(x);
+// a(x);
+// b(5,6);
+
+// switch -case
+
+// let d = 4;
+// switch (d) {
+//   case 0:
+//     day = "Sunday";
+//     console.log(day);
+//     break;
+//   case 1:
+//     day = "Monday";
+//     console.log(day);
+//     break;
+//   case 2:
+//     day = "Tuesday";
+//     console.log(day);
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     console.log(day);
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     console.log(day);
+//     break;
+//   case 5:
+//     day = "Friday";
+//     console.log(day);
+//     break;
+//   case 6:
+//     day = "Saturday";
+//     console.log(day);
+//   default:
+//     console.log(day);
+// }
+
+// Object
+
+const obj = {
+  name: "ashu",
+  roll: 12,
+};
+
+// manipulate object
+obj.name = "changed";
+console.log(obj.name);
