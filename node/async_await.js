500d4d6328bbc663b0b5acf7749a72dfac71ddea
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2024-04-10 09:39:05 +0530

    Delete node directory

--- node/async_await.js
+++
@@ -1,7 +0,0 @@
-async function fetchAPI(){
-    var res = await fetch('https://dummyjson.com/products')
-    var data =await res.json()
-    console.log(data)
-}
-
-fetchAPI();
--- node/index.js
+++
@@ -1,5 +0,0 @@
-(function(){
-    var a=5;
-    var b=6;
-    console.log(a+b);
-})();
