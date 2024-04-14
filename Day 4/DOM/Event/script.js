141ba54e35e75e8c5a983c50ac32bc52c879577d
Ashutosh <mail.ashutosh246@gmail.com>
2024-04-14 18:08:23 +0530

    Day 4

--- Day 4/DOM/Event/script.js
+++ Day 4/DOM/Event/script.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+
  function func(e){
     console.log(e);
 } 
-=======
- function func(e){
-    console.log(e);
-} 
->>>>>>> 59a0b268195fbb671256444ba536d09bf9300fc8
+
--- Day 4/DOM/form/script.js
+++ Day 4/DOM/form/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function printIt(e) {
     e.preventDefault();
     const t = e.target;
@@ -28,35 +28,4 @@ function printIt(e) {
     function print(x=5){
         console.log(`hiii ${x} byee`);
     }
-=======
-function printIt(e) {
-    e.preventDefault();
-    const t = e.target;
-    for (let i = 0; i < t.length; i++) {
-        // if checkbox -> then type =checkbox
-        if (t[i].type === "radio" && t[i].checked) {
-            console.log(t[i].value);
-        }
-        if (t[i].type !== "radio") {
-            console.log(t[i].value)
-        }
-
-    }
-}
-
-
-
-// Higeher order function 
-    // a function that accept fucntion as a parameter or return a function as parameter
-
-    let a = function sum(a=2,b=6){
-        console.log(a+b);
-        return print();
-    }
-
-    
-    function print(x=5){
-        console.log(`hiii ${x} byee`);
-    }
->>>>>>> 59a0b268195fbb671256444ba536d09bf9300fc8
     a()
